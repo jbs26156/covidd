43127f9c3f17fac25791d7e5d3f02f62e71580aa
jbs26156 <jbs26156@uga.edu>
2020-11-23 22:09:42 -0500

    Hey

--- src/main/java/com/csci4050/covid/repository/ContactTraceRepo.java
+++ src/main/java/com/csci4050/covid/repository/ContactTraceRepo.java
@@ -4,8 +4,12 @@ import com.csci4050.covid.entities.AccountEntity;
 import com.csci4050.covid.entities.ContactTraceEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 
 public interface ContactTraceRepo extends CrudRepository<ContactTraceEntity,Long> {
+    Optional<ContactTraceEntity> findById(Long Id);
+
     ContactTraceEntity findByEmail(String email);
     ContactTraceEntity findByBuilding(String building);
     ContactTraceEntity findByAllBuilding(String building);
